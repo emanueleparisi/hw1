03e44e15b54e3ca9ff1071db34d7a91a8e0f3af5
Emanuele Parisi <emanuele.parisi@bsc.es>
2025-04-02 12:15:45 +0200

    Add solution for problem 2

--- src/test/scala/hw1/MajorityCircuitTester.scala
+++ src/test/scala/hw1/MajorityCircuitTester.scala
@@ -8,8 +8,53 @@ class MajorityCircuitTester extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "MajorityCircuit"
   it should "correctly pass tests on all 8 possible inputs" in {
     test(new MajorityCircuit) { dut =>
-      // FILL IN HERE
-			???
+      println("Testing a=0, b=0, c=0")
+      dut.io.a.poke(0.B)
+      dut.io.b.poke(0.B)
+      dut.io.c.poke(0.B)
+      dut.io.out.expect(0.U)
+
+      println("Testing a=0, b=0, c=1")
+      dut.io.a.poke(0.B)
+      dut.io.b.poke(0.B)
+      dut.io.c.poke(1.B)
+      dut.io.out.expect(0.U)
+
+      println("Testing a=0, b=1, c=0")
+      dut.io.a.poke(0.B)
+      dut.io.b.poke(1.B)
+      dut.io.c.poke(0.B)
+      dut.io.out.expect(0.U)
+
+      println("Testing a=0, b=1, c=1")
+      dut.io.a.poke(0.B)
+      dut.io.b.poke(1.B)
+      dut.io.c.poke(1.B)
+      dut.io.out.expect(1.U)
+
+      println("Testing a=1, b=0, c=0")
+      dut.io.a.poke(1.B)
+      dut.io.b.poke(0.B)
+      dut.io.c.poke(0.B)
+      dut.io.out.expect(0.U)
+
+      println("Testing a=1, b=0, c=1")
+      dut.io.a.poke(1.B)
+      dut.io.b.poke(0.B)
+      dut.io.c.poke(1.B)
+      dut.io.out.expect(1.U)
+
+      println("Testing a=1, b=1, c=0")
+      dut.io.a.poke(1.B)
+      dut.io.b.poke(1.B)
+      dut.io.c.poke(0.B)
+      dut.io.out.expect(1.U)
+
+      println("Testing a=1, b=1, c=1")
+      dut.io.a.poke(1.B)
+      dut.io.b.poke(1.B)
+      dut.io.c.poke(1.B)
+      dut.io.out.expect(1.U)
     }
   }
 }
